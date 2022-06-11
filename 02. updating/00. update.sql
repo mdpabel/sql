39fa6a703f2f14dfb13a4918c1a72dc4a0f92d30
mdpabel <mdpabel385@gmail.com>
2022-06-12 02:21:21 +0600

    update

---
+++ 02. updating/00. update.sql
@@ -0,0 +1,27 @@
+-- USE sql_store;
+
+-- --@BLOCK
+-- UPDATE orders
+-- SET shipper_id = 3
+-- WHERE order_id = 3
+
+-- USE sql_invoicing;
+
+--@BLOCK
+-- UPDATE invoices
+-- SET payment_total = 200, payment_date = '2022-01-01'
+-- WHERE invoice_id = 1
+
+-- UPDATE invoices
+-- SET payment_total = invoice_total * 0.5, payment_date = invoice_date
+-- WHERE invoice_id IN (3,4)
+
+USE sql_invoicing;
+
+--@BLOCK
+
+UPDATE invoices
+SET payment_total = 10000
+WHERE client_id IN (SELECT client_id
+                    FROM clients
+                    WHERE state IN ('NY', 'TX'))
